21b5cad86afb2894b6aacc81fbcf799a9e2405b8
Yuffie Hu <yuffieevans@gmail.com>
2022-07-09 18:58:20 -0500

    feat: setup weathercards and weathercard markup and CSS grid and basic logic of rendering time

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import WeatherCards from './WeatherCards';
+import '../index.css';
+import determineTimeOfTheDay from '../utils/time';
 
 /**
  * The main React **App** component.
  */
 const App = () => {
+  // not using state here, assuming the time only gets read every time user refreshes the page
+  const currentHour = new Date().getHours();
+  const timeOfTheDay = determineTimeOfTheDay(currentHour);
+
   return (
     <div className="page">
       <div className="page__wrapper">
         App
+        <WeatherCards timeOfTheDay={timeOfTheDay} />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
