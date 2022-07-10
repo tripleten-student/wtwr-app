db713647d8e3462667c382ff309a15d5ce6a1956
hoanglechau <hoangchau612@gmail.com>
2022-07-10 19:56:27 +0700

    Create error.js

---
+++ backend/utils/error.js
@@ -0,0 +1,4 @@
+module.exports.HTTP_SUCCESS_OK = 200;
+module.exports.HTTP_CLIENT_ERROR_BAD_REQUEST = 400;
+module.exports.HTTP_CLIENT_ERROR_NOT_FOUND = 404;
+module.exports.HTTP_INTERNAL_SERVER_ERROR = 500;
