9fda7817386dc6837dbc6bb7c73908ebad91ceda
hoanglechau <hoangchau612@gmail.com>
2022-07-10 21:23:02 +0700

    Create auth.js

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,50 @@
-/**
- * This module enables API requests related to registration and authorization using API
- * @module Auth
- */
+// Need to update BASE_URL with the registered URL of the project later
+
+class Auth {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  _checkServerResponse(res) {
+    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+  }
+
+  register({ email, password }) {
+    return fetch(`${this._baseUrl}/signup`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify({ email, password }),
+    }).then(this._checkServerResponse);
+  }
+
+  login({ email, password }) {
+    return fetch(`${this._baseUrl}/signin`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify({ email, password }),
+    }).then(this._checkServerResponse);
+  }
+
+  checkToken(token) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: 'GET',
+      headers: {
+        ...this._headers,
+        Authorization: `Bearer ${token}`,
+      },
+    }).then(this._checkServerResponse);
+  }
+}
+
+// Put the registered URL here later
+const BASE_URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000';
+
+const auth = new Auth({
+  baseUrl: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default auth;
