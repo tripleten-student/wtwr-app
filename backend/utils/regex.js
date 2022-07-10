488a25f91c1fa27d0dc0a522f86e71fcfb26089e
hoanglechau <hoangchau612@gmail.com>
2022-07-10 22:26:17 +0700

    Update regex.js with eslint

--- backend/utils/regex.js
+++ backend/utils/regex.js
@@ -1,4 +1,3 @@
-const urlRegExp =
-  /(http(s)?):\/\/(www.)?[a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b(-[a-zA-Z0-9@:%_+.~#?&//=]*)/;
+const urlRegExp = /(http(s)?):\/\/(www.)?[a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b(-[a-zA-Z0-9@:%_+.~#?&//=]*)/;
 
 module.exports = { urlRegExp };
