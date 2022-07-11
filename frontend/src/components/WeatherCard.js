79c1c20f28a23c9a52fae07b82b3ffbb98c3bd8e
Yuffie Hu <yuffieevans@gmail.com>
2022-07-10 21:11:57 -0500

    refactor: with BEM flat style, add more logic to dynamically generate visuals

--- frontend/src/components/WeatherCard.js
+++ frontend/src/components/WeatherCard.js
@@ -1,12 +1,19 @@
-const WeatherCard = ({ time }) => {
+import './WeatherCard.css';
+
+// WeatherCard component is individual weathercard that displays morning, afternoon, evening and overnight weather report
+const WeatherCard = ({ displayedTime, timeOfTheDay }) => {
+  const dayOrNight = displayedTime === 'Morning' || displayedTime === 'Afternoon' ? 'day' : 'night';
+  //const weather = useContext()
+  //this is just a placeholder for testing
+  const weather = 'storm';
   return (
     <div
-      className={`weathercard ${
-        time === 'Morning' || time === 'Afternoon' ? 'weathercard_day' : 'weathercard_night'
-      }`}
+      className={`weathercard weathercard_${dayOrNight} ${
+        displayedTime.toLowerCase() === timeOfTheDay && 'weathercard_elongated'
+      } weathercard_weather_${dayOrNight}-${weather}`}
     >
       <div className="weathercard__info-container">
-        <p className="weathercard__time">{time}</p>
+        <p className="weathercard__time">{displayedTime}</p>
         <p className="weathercard__temperature">73°</p>
         <p className="weathercard__description">Chance of rain: 70%</p>
       </div>
--- frontend/src/components/WeatherCards.js
+++ frontend/src/components/WeatherCards.js
@@ -1,6 +1,7 @@
 import WeatherCard from './WeatherCard';
+import './WeatherCards.css';
 
-// WeatherCards is a container that holds all four WeatherCards. It is a CSS grid
+// WeatherCards is a container that holds all four WeatherCards. It is a CSS flexbox
 // assuming celsius/fahrenheit, temperature, and weather parameters will be passed down from App.js using global context, I'm not including them as one of the parameters here
 
 // description parameter comes from the API
@@ -8,10 +9,10 @@ const WeatherCards = ({ timeOfTheDay, description }) => {
   return (
     // modifier to adjust different CSS grid layout according to different times of the day
     <div className={`weathercards weathercards_${timeOfTheDay}`}>
-      <WeatherCard time="Morning" />
-      <WeatherCard time="Afternoon" />
-      <WeatherCard time="Evening" />
-      <WeatherCard time="Overnight" />
+      <WeatherCard displayedTime="Morning" timeOfTheDay={timeOfTheDay} />
+      <WeatherCard displayedTime="Afternoon" timeOfTheDay={timeOfTheDay} />
+      <WeatherCard displayedTime="Evening" timeOfTheDay={timeOfTheDay} />
+      <WeatherCard displayedTime="Overnight" timeOfTheDay={timeOfTheDay} />
     </div>
   );
 };
