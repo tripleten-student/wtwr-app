6ee9617b4ab2d2903190f08aed06835549973c80
Samm96 <sihorsch96@gmail.com>
2022-07-11 17:19:41 -0400

    Fixed Header

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import Header from './Header';
+import './App.css';
+import WeatherCards from '../WeatherCards/WeatherCards';
+import { determineTimeOfTheDay } from '../../utils/weatherCards';
+import Main from '../Main/Main';
+import Footer from '../Footer/Footer';
+import Header from '../components/Header/Header';
 
 /**
  * The main React **App** component.
  */
 const App = () => {
+  // not using state here, assuming the time only gets read every time user refreshes the page
+  const currentHour = new Date().getHours();
+  const timeOfTheDay = determineTimeOfTheDay(currentHour);
+
   return (
     <div className="page">
       <div className="page__wrapper">
-        <Header />
         App
+        <Header />
+        <WeatherCards timeOfTheDay={timeOfTheDay} description="Data from Weather API" />
+        <Main/>
+        <Footer />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
