02249c18c17d80bf3d6a5e56c1b0fdd9feb86bca
Shraddha <mailmeatshraddha@gmail.com>
2022-07-11 15:32:13 -0400

    feat: add context for current temp unit

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import WeatherCards from '../WeatherCards/WeatherCards';
+import CurrentTemperatureUnitContext from '../../contexts/CurrentTemperatureUnitContext';
 import { determineTimeOfTheDay } from '../../utils/weatherCards';
 
 /**
  * The main React **App** component.
  */
 const App = () => {
+  const [currentTemperatureUnit, setCurrentTemperatureUnit] = React.useState('F');
+
+  const handleToggleSwitchChange = () => {
+    currentTemperatureUnit === 'F'
+      ? setCurrentTemperatureUnit('C')
+      : setCurrentTemperatureUnit('F');
+  };
+
   // not using state here, assuming the time only gets read every time user refreshes the page
   const currentHour = new Date().getHours();
   const timeOfTheDay = determineTimeOfTheDay(currentHour);
@@ -14,8 +23,11 @@ const App = () => {
   return (
     <div className="page">
       <div className="page__wrapper">
-        App
-        <WeatherCards timeOfTheDay={timeOfTheDay} description="Data from Weather API" />
+        <CurrentTemperatureUnitContext.Provider
+          value={{ currentTemperatureUnit, handleToggleSwitchChange }}
+        >
+          <WeatherCards timeOfTheDay={timeOfTheDay} description="Data from Weather API" />
+        </CurrentTemperatureUnitContext.Provider>
       </div>
     </div>
   );
