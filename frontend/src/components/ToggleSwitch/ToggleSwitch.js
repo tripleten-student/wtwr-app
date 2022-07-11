564652ca4a3e5c3d3545e2ad49340cf4f908f9be
Shraddha <mailmeatshraddha@gmail.com>
2022-07-11 15:31:48 -0400

    feat: add ToggleSwitch component

--- frontend/src/components/ToggleSwitch/ToggleSwitch.js
+++ frontend/src/components/ToggleSwitch/ToggleSwitch.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import './ToggleSwitch.css';
+import CurrentTemperatureUnitContext from '../../contexts/CurrentTemperatureUnitContext';
 
 /**
  * The **ToggleSwitch** component renders a switch used to toggle temperature units.
@@ -6,14 +8,24 @@ import './ToggleSwitch.css';
  * @author [Shraddha](https://github.com/5hraddha)
  */
 const ToggleSwitch = () => {
+  const { currentTemperatureUnit, handleToggleSwitchChange } = React.useContext(
+    CurrentTemperatureUnitContext
+  );
+
   return (
     <div className="toggleswitch">
       <label className="toggleswitch__label">
-        <input className="toggleswitch_checkbox toggleswitch__checkbox_state_hidden" type="checkbox" name="toggleswitch-checkbox" value="1" />
+        <input
+          className="toggleswitch_checkbox toggleswitch__checkbox_state_hidden"
+          type="checkbox"
+          name="toggleswitch-checkbox"
+          value={currentTemperatureUnit}
+          onChange={handleToggleSwitchChange}
+        />
         <span className="toggleswitch__checkbox toggleswitch__checkbox_state_visible"></span>
       </label>
     </div>
   );
-}
+};
 
 export default ToggleSwitch;
