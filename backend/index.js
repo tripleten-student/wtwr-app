6e6c07f95735cc52277ae6724d79174bd12fe64a
Devin Jaggernauth <neexis@gmail.com>
2022-07-10 22:25:38 -0400

    setup cors and helmet, mainRouter

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 require('dotenv').config();
-
-const PORT = process.env.PORT || 5000;
+// const mongoose = require('mongoose');
+const helmet = require('helmet');
+const cors = require('cors');
 
 const app = express();
+app.use(helmet());
+app.use(cors);
+app.options('*', cors());
+
+const { PORT = 3000, NODE_ENV, MONGO_URI } = process.env;
+
+// mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : localdb);
+
+app.use(express.json());
+
+const mainRouter = require('./routes/index');
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
---
+++ backend/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const userRouter = require('./users');
+const clothingRouter = require('./clothing');
+
+const router = express.Router();
+
+router.use('/users', userRouter);
+router.use('/articles', clothingRouter);
+
+module.exports = router;
