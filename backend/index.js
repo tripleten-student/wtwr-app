9ce826bbbb3b2fc0bcbd87cf5497b28398d9764f
Devin Jaggernauth <neexis@gmail.com>
2022-07-10 22:51:19 -0400

    install winston and celebrate packages

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 require('dotenv').config();
 // const mongoose = require('mongoose');
+const { errors } = require('celebrate')
 const helmet = require('helmet');
 const cors = require('cors');
+const { requestLogger, errorLogger } = require('./middleware/logger');
+const auth = require('./middleware/auth');
 
 const app = express();
 app.use(helmet());
@@ -20,3 +23,10 @@ const mainRouter = require('./routes/index');
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
+
+app.use(errorLogger);
+
+app.use(errors());
+
+// ** Error handler not yet set up **
+// app.use(errorHandler);
