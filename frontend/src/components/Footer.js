df23f00469dcf2c396423781a3f3298fcc9ae1e3
Nuriya Akhmedova <nuriya_a@yahoo.com>
2022-07-11 12:35:15 -0400

    feature footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,15 @@
-function Footer() {
+/**
+ * The **Footer** component displays copyright and year information
+ *
+ * @author [Nuriya](https://github.com/NuriyaAkh)
+ */
+
+const Footer = () => {
   return (
-    <footer className='footer'>
-      <p className='footer__text'>© Developed by Practicum Students</p>
-      <p className='footer__year'>2022</p>
+    <footer className="footer">
+      <p className="footer__copyright">© Developed by Practicum Students</p>
+      <p className="footer__year">{new Date().getFullYear()}</p>
     </footer>
   );
-}
+};
 export default Footer;
