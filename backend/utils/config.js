5fc90378750d2627ab4d355ed789a5d23e9d5675
Devin Jaggernauth <neexis@gmail.com>
2022-07-10 23:27:42 -0400

    utils config file

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,4 +1,4 @@
-const localdb = 'url to local mongo db'
-const devkey = 'dev-secret key'
+const localdb = 'url to local mongo db';
+const devkey = 'dev-secret key';
 
-module.exports = { localdb, devkey }
+module.exports = { localdb, devkey };
