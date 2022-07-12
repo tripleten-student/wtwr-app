bb353d0c635269e1f6b27b88b431101c39ba401b
Samm96 <sihorsch96@gmail.com>
2022-07-11 22:04:14 -0400

    Add: some logic in navigationjs / if no avatar img / at fixed height

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Navigation.css';
-import ToggleSwitch from '../ToggleSwitch/ToggleSwitch';
 
 /**
  * The Navigation component includes the ToggleSwitch component
@@ -8,12 +7,26 @@ import ToggleSwitch from '../ToggleSwitch/ToggleSwitch';
  * @author [Sam](https://github.com/Samm96)
  */
 
-function Navigation() {
+function Navigation({ isLoggedIn, username, hasAvatar }) {
+  const defaultUser = 'Terrence Tegegne';
+
   return (
     <div className="navigation">
-      <button className="navigation__button">Sign Up</button>
-      <button className="navigation__button">Log In</button>
-      <img className="navigation__user" src={ require('../../images/avatar-default.png') } alt="user avatar" />
+      <button className="navigation__button">{ isLoggedIn ? (username || defaultUser) : 'Sign In'}</button>
+      <button className="navigation__button">{ isLoggedIn ? '+ Add clothes' : 'Log In'}</button>
+      {isLoggedIn ? (
+        <img
+          className="navigation__user"
+          src={
+            hasAvatar
+              ? require('../../images/avatar-default.png')
+              : require('../../images/avatar-false.png')
+          }
+          alt="user avatar"
+        />
+      ) : (
+        <img className="navigation__user navigation__user_signin" alt="user avatar" />
+      )}
     </div>
   );
 }
