97ce0fafaa15b1771c8573f6cf7549f94ae786f9
Yuffie Hu <yuffieevans@gmail.com>
2022-07-13 09:44:04 -0500

    feat: add functions to process weather data

--- frontend/src/components/WeatherCard/WeatherCard.js
+++ frontend/src/components/WeatherCard/WeatherCard.js
@@ -1,5 +1,5 @@
 import './WeatherCard.css';
-import { shortenDescription } from '../../utils/weatherCards';
+import { shortenDescription, categorizeWeatherTypeForImage } from '../../utils/weatherCards';
 
 /**
  * WeatherCard component is individual weathercard that displays morning, afternoon, evening and overnight weather report
@@ -8,26 +8,35 @@ import { shortenDescription } from '../../utils/weatherCards';
 
 const WeatherCard = ({ displayedTime, timeOfTheDay }) => {
   const dayOrNight = displayedTime === 'Morning' || displayedTime === 'Afternoon' ? 'day' : 'night';
+
   /**
-   * const {weather, description} = useContext()
+   * const {weatherCondition, description} = useContext()
    * this is just a placeholder for testing
    */
-  const weather = 'cloudy';
-  const description = 'Chance of Rain: 70%';
-  const shortenedDescription = shortenDescription(description);
+  const weatherCondition = 'Clear';
+  const categorizedWeather = categorizeWeatherTypeForImage(weatherCondition);
+  console.log(categorizedWeather);
+  const backgroundImage =
+    categorizedWeather === 'not found'
+      ? ''
+      : `weathercard_weather_${dayOrNight}-${categorizedWeather}`;
+
+  const description = weatherCondition;
+  // const shortenedDescription = shortenDescription(description);
   return (
     <div
       className={`weathercard weathercard_${dayOrNight} ${
         displayedTime.toLowerCase() === timeOfTheDay
           ? 'weathercard_elongated'
           : 'weathercard_overlay'
-      } weathercard_weather_${dayOrNight}-${weather}`}
+      } ${backgroundImage}`}
     >
       <div className="weathercard__info-container">
         <p className="weathercard__time">{displayedTime}</p>
         <p className="weathercard__temperature">73°</p>
         <p className="weathercard__description">
-          {displayedTime.toLowerCase() === timeOfTheDay ? description : shortenedDescription}
+          {description}
+          {/* {displayedTime.toLowerCase() === timeOfTheDay ? description : shortenedDescription} */}
         </p>
       </div>
     </div>
--- frontend/src/utils/weatherCards.js
+++ frontend/src/utils/weatherCards.js
@@ -1,3 +1,88 @@
+const uniqueDayAndNightConditions = [
+  'Sunny',
+  'Clear',
+  'Partly cloudy',
+  'Cloudy',
+  'Overcast',
+  'Mist',
+  'Patchy rain possible',
+  'Patchy snow possible',
+  'Patchy sleet possible',
+  'Patchy freezing drizzle possible',
+  'Thundery outbreaks possible',
+  'Blowing snow',
+  'Blizzard',
+  'Fog',
+  'Freezing fog',
+  'Patchy light drizzle',
+  'Light drizzle',
+  'Freezing drizzle',
+  'Heavy freezing drizzle',
+  'Patchy light rain',
+  'Light rain',
+  'Moderate rain at times',
+  'Moderate rain',
+  'Heavy rain at times',
+  'Heavy rain',
+  'Light freezing rain',
+  'Moderate or heavy freezing rain',
+  'Light sleet',
+  'Moderate or heavy sleet',
+  'Patchy light snow',
+  'Light snow',
+  'Patchy moderate snow',
+  'Moderate snow',
+  'Patchy heavy snow',
+  'Heavy snow',
+  'Ice pellets',
+  'Light rain shower',
+  'Moderate or heavy rain shower',
+  'Torrential rain shower',
+  'Light sleet showers',
+  'Moderate or heavy sleet showers',
+  'Light snow showers',
+  'Moderate or heavy snow showers',
+  'Light showers of ice pellets',
+  'Moderate or heavy showers of ice pellets',
+  'Patchy light rain with thunder',
+  'Moderate or heavy rain with thunder',
+  'Patchy light snow with thunder',
+  'Moderate or heavy snow with thunder',
+];
+
+function categorizeWeatherTypeForImage(description) {
+  description = description.toLowerCase();
+  if (
+    description.includes('rain') ||
+    (description.includes('drizzle') && !description.includes('freezing'))
+  ) {
+    return 'rainy';
+  } else if (description.includes('sun') || description.includes('clear')) {
+    return 'sunny';
+  } else if (description.includes('fog') || description.includes('mist')) {
+    return 'foggy';
+  } else if (
+    description.includes('pellet') ||
+    description.includes('sleet') ||
+    description.includes('freezing drizzle')
+  ) {
+    return 'sleet';
+  } else if (description.includes('thunder')) {
+    return 'stormy';
+  } else if (description.includes('snow') || description.includes('blizzard')) {
+    return 'snowy';
+  } else if (description.includes('cloud')) {
+    return 'cloudy';
+  } else {
+    return 'not found';
+  }
+}
+// use this for testing, you will see all 49 items categoried in 7 conditions
+// console.log(
+//   uniqueDayAndNightConditions.map((description) =>
+//     categorizeWeatherTypeForImage(description)
+//   )
+// );
 function determineTimeOfTheDay(currentHour) {
   if (currentHour >= 5 && currentHour < 12) {
     return 'morning';
@@ -14,4 +99,4 @@ function shortenDescription(description) {
   return description.match(regex)[0];
 }
 
-export { determineTimeOfTheDay, shortenDescription };
+export { determineTimeOfTheDay, shortenDescription, categorizeWeatherTypeForImage };
