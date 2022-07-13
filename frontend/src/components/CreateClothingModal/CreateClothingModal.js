d78d85a1b2a604ec86129fe04770dc443a4b6849
pstaal <peter.staal@gmail.com>
2022-07-13 09:32:31 +0200

    set up boilerplate component

---
+++ frontend/src/components/CreateClothingModal/CreateClothingModal.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ModalWithForm from '../ModalWithForm/ModalWithForm';
+import PropTypes from 'prop-types';
+/**
+ * The **CreateClothingModal** component will let users add new clothes to the database.
+ * It uses the ModalWithForm component.
+ *
+ *  @author [Peter Staal](https://github.com/pstaal)
+ */
+
+ const CreateClothingModal = () => {
+
+    return (
+        <ModalWithForm>
+            
+        </ModalWithForm>
+    )
+ };
+
+
+ export default CreateClothingModal;
