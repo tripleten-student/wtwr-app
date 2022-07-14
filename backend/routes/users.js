2dc755d6b1569fd84fee04a3d67013b406094209
hoanglechau <hoangchau612@gmail.com>
2022-07-14 14:24:12 +0700

    Create routes for users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,3 +1,31 @@
-const express = require('express');
+const router = require('express').Router();
 
-const router = express.Router();
+/**
+ * The **users** module contains the routes for users
+ *
+ * @author [Hoang Le Chau](https://github.com/hoanglechau)
+ */
+
+const {
+  getUsers,
+  getCurrentUser,
+  getUserById,
+  updateName,
+  updateAvatar,
+  updatePassword,
+  deleteUser,
+} = require('../controllers/users');
+const {
+  validateRequestAuth,
+  validateUserId,
+} = require('../middleware/validation');
+
+router.get('/', validateRequestAuth, getUsers);
+router.get('/me', validateRequestAuth, getCurrentUser);
+router.get('/:id', validateRequestAuth, validateUserId, getUserById);
+router.patch('/me', validateRequestAuth, updateName);
+router.patch('/me/avatar', validateRequestAuth, updateAvatar);
+router.patch('/me/password', validateRequestAuth, updatePassword);
+router.delete('/me', validateRequestAuth, deleteUser);
+
+module.exports = router;
