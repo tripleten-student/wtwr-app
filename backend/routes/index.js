36c6ef66034d6b9ef89f6107fefccd042d389893
hoanglechau <hoangchau612@gmail.com>
2022-07-14 14:24:44 +0700

    Update the main routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,23 @@
 const express = require('express');
-// const userRouter = require('./users');
-const clothingRouter = require('./clothing');
 
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { createUser, login } = require('../controllers/users');
+const { validateLogin, validateUser } = require('../middleware/validation');
 
-// router.use('/users', userRouter);
+const userRouter = require('./users');
+const clothingRouter = require('./clothing');
+const NotFoundError = require('../errors/not-found-error');
+
+router.post('/signin', validateLogin, login);
+router.post('/signup', validateUser, createUser);
+
+router.use(auth);
+router.use('/users', userRouter);
 router.use('/clothing', clothingRouter);
+
+router.use((req, res, next) => {
+  next(new NotFoundError('Requested resource not found'));
+});
 
 module.exports = router;
