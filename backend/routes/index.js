741c94291bb75e5371a7cfd3cce0160ba753a6f3
hoanglechau <hoangchau612@gmail.com>
2022-07-15 15:53:47 +0700

    Disable authorization and delete extra code

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,23 +8,20 @@ const express = require('express');
  */
 
 const router = express.Router();
-const auth = require('../middleware/auth');
+// const auth = require('../middleware/auth');
 const { createUser, login } = require('../controllers/users');
 const { validateLogin, validateUser } = require('../middleware/validation');
 
 const userRouter = require('./users');
 const clothingRouter = require('./clothing');
-const NotFoundError = require('../errors/not-found-error');
 
 router.post('/signin', validateLogin, login);
 router.post('/signup', validateUser, createUser);
 
-router.use(auth);
+// Authorization is currently disabled for testing purpose
+// router.use(auth);
+
 router.use('/users', userRouter);
 router.use('/clothing', clothingRouter);
-
-router.use((req, res, next) => {
-  next(new NotFoundError('Requested resource not found'));
-});
 
 module.exports = router;
