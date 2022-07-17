15bbf5d34a5622c4f4cb43f058843f35bacb864c
hoanglechau <hoangchau612@gmail.com>
2022-07-17 17:52:13 +0700

    Make name and avatar optional in user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -26,13 +26,11 @@ const userSchema = new mongoose.Schema({
   },
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
   },
   avatar: {
     type: String,
-    required: true,
     validate: {
       validator: validator.isURL,
       message: 'You must enter a valid URL',
