5163c65f0d7caffc419616d17c4b0c8cd1f83682
Santiago SR <santisachezromero@gmail.com>
2022-07-19 18:38:35 -0300

    feature: Profile page

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -14,64 +14,72 @@ function Profile({ cardData, onCardLike }) {
       <section className="profile-sidebar">
         <SideBar />
       </section>
-      <div className="profile-clothes">
-        <div className="profile-section__info">
-          <h3 className="profile-section__title">Accessories</h3>
-          <button className="profile-section__button"> +Add new</button>
+      <section className="profile-clothes">
+        <div className="profile-section">
+          <div className="profile-section__info">
+            <h3 className="profile-section__title">Accessories</h3>
+            <button className="profile-section__button"> +Add new</button>
+          </div>
+          <ul className="profile-clothes__section Accessories">
+            {accessories.map((card) => (
+              <ClothingCard
+                name="T-shirt"
+                // please test with empty string to see the default image show up on card with "add your photo" button
+                cardData={card}
+                onCardLike={onCardLike}
+              />
+            ))}
+          </ul>
         </div>
-        <section className="profile-clothes__section Accessories">
-          {accessories.map((card) => (
-            <ClothingCard
-              name="T-shirt"
-              // please test with empty string to see the default image show up on card with "add your photo" button
-              cardData={card}
-              onCardLike={onCardLike}
-            />
-          ))}
-        </section>
-        <div className="profile-section__info">
-          <h3 className="profile-section__title">Tops and outerwear</h3>
-          <button className="profile-section__button"> +Add new</button>
+        <div className="profile-section">
+          <div className="profile-section__info">
+            <h3 className="profile-section__title">Tops and outerwear</h3>
+            <button className="profile-section__button"> +Add new</button>
+          </div>
+          <ul className="profile-clothes__section Topsandoutwear">
+            {topsandoutwear.map((card) => (
+              <ClothingCard
+                name="T-shirt"
+                // please test with empty string to see the default image show up on card with "add your photo" button
+                cardData={card}
+                onCardLike={onCardLike}
+              />
+            ))}
+          </ul>
         </div>
-        <section className="profile-clothes__section Topsandoutwear">
-          {topsandoutwear.map((card) => (
-            <ClothingCard
-              name="T-shirt"
-              // please test with empty string to see the default image show up on card with "add your photo" button
-              cardData={card}
-              onCardLike={onCardLike}
-            />
-          ))}
-        </section>
-        <div className="profile-section__info">
-          <h3 className="profile-section__title">Bottoms</h3>
-          <button className="profile-section__button"> +Add new</button>
+        <div className="profile-section">
+          <div className="profile-section__info">
+            <h3 className="profile-section__title">Bottoms</h3>
+            <button className="profile-section__button"> +Add new</button>
+          </div>
+          <ul className="profile-clothes__section Topsandoutwear">
+            {bottoms.map((card) => (
+              <ClothingCard
+                name="T-shirt"
+                // please test with empty string to see the default image show up on card with "add your photo" button
+                cardData={card}
+                onCardLike={onCardLike}
+              />
+            ))}
+          </ul>
         </div>
-        <section className="profile-clothes__section Topsandoutwear">
-          {bottoms.map((card) => (
-            <ClothingCard
-              name=""
-              // please test with empty string to see the default image show up on card with "add your photo" button
-              cardData={card}
-              onCardLike={onCardLike}
-            />
-          ))}
-        </section>
-        <div className="profile-section__info">
-          <h3 className="profile-section__title">Shoes</h3>
-          <button className="profile-section__button"> +Add new</button>
+        <div className="profile-section">
+          <div className="profile-section__info">
+            <h3 className="profile-section__title">Shoes</h3>
+            <button className="profile-section__button"> +Add new</button>
+          </div>
+          <ul className="profile-clothes__section Topsandoutwear">
+            {shoes.map((card) => (
+              <ClothingCard
+                name="T-shirt"
+                // please test with empty string to see the default image show up on card with "add your photo" button
+                cardData={card}
+                onCardLike={onCardLike}
+              />
+            ))}
+          </ul>
         </div>
-        <section className="profile-clothes__section Topsandoutwear">
-          {shoes.map((card) => (
-            <ClothingCard
-              name="T-shirt"
-              // please test with empty string to see the default image show up on card with "add your photo" button
-              cardData={card}
-              onCardLike={onCardLike}
-            />
-          ))}
-        </section>
-      </div>
+      </section>
     </div>
   );
 }
