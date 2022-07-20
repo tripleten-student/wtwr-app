2c3fdca866eb6f9f9e91e6e21a072c365e137cff
Devin Jaggernauth <neexis@gmail.com>
2022-07-20 10:39:54 -0400

    remove use effect from button

--- frontend/src/components/ClothingSelectorButton/ClothingSelectorButton.js
+++ frontend/src/components/ClothingSelectorButton/ClothingSelectorButton.js
@@ -5,13 +5,9 @@ const ClothingSelectorButton = ({ onItemSelect, item, clothingPreferences }) => 
   const { name, value } = item;
   const [isSelected, setIsSelected] = useState(clothingPreferences.includes(value));
 
-  useEffect(()=>{
-    setIsSelected(clothingPreferences.includes(value))
-  },[clothingPreferences, value])
-
   const handleClick = () => {
     onItemSelect(value);
-    setIsSelected(clothingPreferences.includes(value))
+    setIsSelected(!isSelected)
   };
 
   return (
