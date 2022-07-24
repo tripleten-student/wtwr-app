f212bd3537d750b2c22cde559f4a2d7e3e171d11
Samm96 <sihorsch96@gmail.com>
2022-07-23 20:53:25 -0400

    Add: component css

--- frontend/src/components/ShowClothingModal/ShowClothingModal.js
+++ frontend/src/components/ShowClothingModal/ShowClothingModal.js
@@ -3,31 +3,30 @@ import './ShowClothingModal.css';
 import Modal from '../Modal/Modal';
 import { NavLink } from 'react-router-dom';
 
- /**
+/**
  * The ShowClothingModal component
  *
  * @author [Sam](https://github.com/Samm96)
  *
  */
 
-function ShowClothingModal({ clothingType, tempType, tempDegree, tempUnit }) { 
-    return (
-        <Modal 
-            name='ShowClothingModal'
-            position='middle'
-            width='wide'
-            isOpen='modal_opened'
-        >
-            <div className="clothing-modal__container">
-                <img src={require('../../images/Clothes/letter-embroidered-baseball-cap.png')} alt='clothing' className="clothing-modal__image" />
-                <div className="clothing-modal__text-container">
-                    <p className="clothing-modal__text">Type: Accessories</p>
-                    <p className="clothing-modal__text">Temperature: Hot (70° F+)</p>
-                    <p className="clothing-modal__link">Edit</p>
-                </div>
-            </div>
-        </Modal>
-    )
- }
+function ShowClothingModal({ clothingType, tempType, tempDegree, tempUnit, path }) {
+  return (
+    <Modal name="ShowClothingModal" position="middle" width="wide" isOpen="modal_opened">
+      <div className="clothing-modal__container">
+        <img
+          src={require('../../images/Clothes/letter-embroidered-baseball-cap.png')}
+          alt="clothing"
+          className="clothing-modal__image"
+        />
+          <div className="clothing-modal__text-container">
+            <p className="clothing-modal__text">Type: Accessories</p>
+            <p className="clothing-modal__text">Temperature: Hot (70° F+)</p>
+            <a href={path} className="clothing-modal__link">Edit</a>
+          </div>
+      </div>
+    </Modal>
+  );
+}
 
 export default ShowClothingModal;
