988837869cc7d28516846aee84797ee047beefaa
Samm96 <sihorsch96@gmail.com>
2022-07-24 20:14:38 -0400

    Adjust: configured Headerjs code

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -15,18 +15,20 @@ import ToggleSwitch from '../ToggleSwitch/ToggleSwitch';
 ** The options of this method indicate the month should be the full word and date should return
 ** the numeric version of the date.
 
-more info here: <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat#options> 
+more info here: 
+<https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat#options> 
 */
 
 function Header({ currentLocation, children }) {
-
-  const currentDate = new Date().toLocaleString('default', {month: 'long', day: 'numeric'});
+  const currentDate = new Date().toLocaleString('default', { month: 'long', day: 'numeric' });
 
   return (
     <header className="header">
       <div className="header__container">
         <Logo />
-        <p className="header__date">{currentDate || 'June 15'}, {currentLocation || 'New York'}</p>
+        <p className="header__date">
+          {currentDate || 'June 15'}, {currentLocation || 'New York'}
+        </p>
       </div>
       <div className="header__navContainer">
         <ToggleSwitch />
