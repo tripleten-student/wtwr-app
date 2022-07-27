7d714c6e7bd51d10377096a537e7cdaf3af3c5d2
hoanglechau <hoangchau612@gmail.com>
2022-07-27 13:01:24 +0700

    Create the settings file for cors

---
+++ backend/utils/corsSettings.js
@@ -0,0 +1,14 @@
+/**
+ * The **corsSettings** module contains the settings for CORS
+ *
+ * @author [Hoang Le Chau](https://github.com/hoanglechau)
+ */
+
+// add all registered URLs of the project to 'allowedCors' later
+const allowedCors = ['localhost:3000'];
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+module.exports = {
+  allowedCors,
+  DEFAULT_ALLOWED_METHODS,
+};
