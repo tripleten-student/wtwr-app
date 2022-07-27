95501efffe0c8cc5ff707903f525fdd03cff857a
hoanglechau <hoangchau612@gmail.com>
2022-07-27 13:03:14 +0700

    Add the limiter middleware and cors headers

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,12 @@ const helmet = require('helmet');
 const cors = require('cors');
 const { requestLogger, errorLogger } = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
+const limiter = require('./middleware/limiter');
 const { localdb } = require('./utils/config');
+const {
+  allowedCors,
+  DEFAULT_ALLOWED_METHODS,
+} = require('./utils/corsSettings');
 const mainRouter = require('./routes/index');
 
 /**
@@ -22,11 +27,20 @@ const { PORT = 4000, NODE_ENV, MONGO_URI } = process.env;
 
 mongoose.connect(NODE_ENV === 'production' ? MONGO_URI : localdb);
 
+app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-
 app.use(helmet());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', allowedCors);
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  return next();
+});
 
 app.use(cors());
 app.options('*', cors());
