15e6e3ee379b93977f3916abbb1b62181ee077f3
Shraddha <mailmeatshraddha@gmail.com>
2022-07-27 16:44:09 -0400

    remove useContext hook

--- frontend/src/components/EditClothingModal/EditClothingModal.js
+++ frontend/src/components/EditClothingModal/EditClothingModal.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useContext } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import ModalWithForm from '../ModalWithForm/ModalWithForm';
 import Dropdown from '../Dropdown/Dropdown';
