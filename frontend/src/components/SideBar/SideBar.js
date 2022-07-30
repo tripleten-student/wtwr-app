2f1c487261129d5a7c3879e6b136b8f8e3ca21a1
Yuffie Hu <yuffieevans@gmail.com>
2022-07-29 19:36:32 -0500

    add current user context to main

--- frontend/src/components/SideBar/SideBar.js
+++ frontend/src/components/SideBar/SideBar.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './SideBar.css';
 import PropTypes from 'prop-types';
-
-import avatar from '../../images/Avatars/elise.png';
+import CurrentUserContext from '../../contexts/CurrentUserContext';
 
 /**
  * The **SideBar** component displays all the
@@ -18,20 +17,20 @@ const SideBar = ({
   onLogOutClick,
   onDeleteProfileClick,
 }) => {
-  // we will add the  context and use it to retrive the avatar and name of the user
-  //  ADD LATER const currentUser = React.useContext(CurrentUserContext);
-  //   asd
-
+  const currentUser = React.useContext(CurrentUserContext);
+  if (!currentUser) return null;
+  const { username, avatar } = currentUser;
   return (
     <div className="sidebar">
       <div className="sidebar__user">
         <img
           src={avatar} // TO ADD LATER {currentUser.avatar}
           className="sidebar__user-avatar"
-          alt="name of user" // TO ADD LATER {currentUser.name}
+          alt={`${username}'s headshot`} // TO ADD LATER {currentUser.name}
         />
         <p className="sidebar__user-name">
-          Terrence Tegegne{/* // TO ADD LATER {currentUser.name} */}{' '}
+          {username}
+          {/* // TO ADD LATER {currentUser.name} */}{' '}
         </p>
       </div>
       <ul className="sidebar__links">
