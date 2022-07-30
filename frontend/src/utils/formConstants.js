9a0519ed443ec59801fc34f947c193608bda02fd
Shraddha <mailmeatshraddha@gmail.com>
2022-07-29 20:47:47 -0400

    feat: add different contants for C & F temperature

--- frontend/src/utils/formConstants.js
+++ frontend/src/utils/formConstants.js
@@ -14,14 +14,28 @@ const clothingItems = [
   { name: 'Down Jacket', value: 'down-jacket' },
 ];
 
-const weatherTypes = [
-  { name: 'Extremely Cold (-22F° to 32F°)', value: 'extremely-cold' },
-  { name: 'Cold (33F° to 53F°)', value: 'cold' },
-  { name: 'Cool (54F° to 65F°)', value: 'cool' },
-  { name: 'Optimal (66F° to 77F°)', value: 'optimal' },
-  { name: 'Warm (78F° to 85F°)', value: 'warm' },
-  { name: 'Hot (86F° to 96F°)', value: 'hot' },
-  { name: 'Extreme (97F°)', value: 'extreme' },
+const weatherTypesInFahrenheit = [
+  { name: 'Extremely Cold (-22°F to 32°F)', value: 'extremely-cold' },
+  { name: 'Cold (33°F to 53°F)', value: 'cold' },
+  { name: 'Cool (54°F to 65°F)', value: 'cool' },
+  { name: 'Optimal (66°F to 77°F)', value: 'optimal' },
+  { name: 'Warm (78°F to 85°F)', value: 'warm' },
+  { name: 'Hot (86°F to 96°F)', value: 'hot' },
+  { name: 'Extreme (97°F+)', value: 'extreme' },
 ];
 
-export { clothingItems, weatherTypes };
+const weatherTypesInCelcius = [
+  { name: 'Extremely Cold (-30°C to 0°C)', value: 'extremely-cold' },
+  { name: 'Cold (1°C to 12°C)', value: 'cold' },
+  { name: 'Cool (13°C to 18°C)', value: 'cool' },
+  { name: 'Optimal (19°C to 25°C)', value: 'optimal' },
+  { name: 'Warm (26°C to 30°C)', value: 'warm' },
+  { name: 'Hot (31°C to 35°C)', value: 'hot' },
+  { name: 'Extreme (36°C+)', value: 'extreme' },
+];
+
+export {
+  clothingItems,
+  weatherTypesInFahrenheit,
+  weatherTypesInCelcius,
+};
